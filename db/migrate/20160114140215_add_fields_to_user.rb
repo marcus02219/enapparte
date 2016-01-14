6a178cefd4c37f0e57ecac0f357a1c9cee604471
Antoine Berton <antoine@angeltech.io>
2016-01-14 14:03:11 +0000

    added fields to user

---
+++ db/migrate/20160114140215_add_fields_to_user.rb
@@ -0,0 +1,7 @@
+class AddFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :surname, :string
+    add_column :users, :gender, :integer
+  end
+end
