de9ec3440689c0e8fef8455fd4295096c52ddb41
Sam Peacey <sam.peacey@gmail.com>
2016-01-15 18:16:32 +0900

    hide alerts when showing the modal

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,13 @@
-$(document).ready(function(){'use strict';$('nav[role="navigation"]').on('shown.bs.collapse',function(){$('#dropdownMenu1').trigger('click');});});
+$(document).ready( function(){
+  'use strict';
+  $('nav[role="navigation"]').on('shown.bs.collapse',function(){
+    $('#dropdownMenu1').trigger('click');
+  });
+
+  $('.modal').on('show.bs.modal', function() {
+    $('.flash-messages .alert').alert('close');
+  });
+});
 
 
 
