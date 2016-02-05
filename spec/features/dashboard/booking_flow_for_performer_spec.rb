19b5623c8bd36e623bdedc83fe114d295c31c0ca
Oleg A. Bavaev <jesoba7@gmail.com>
2016-02-05 14:26:32 +1000

    added spec

---
+++ spec/features/dashboard/booking_flow_for_performer_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+# As a performer, I should be able to see who has booked one of my shows and confirm or refuse
+# booking requests, so that user who have booked with me know if I’m available or not
+
+feature 'Booking flow for performer' do
+
+  scenario do
+
+  end
+
+end
