b5fb10ab8d31fdcbf3bfc4a33a4b1a14d7e847c8
Oleg A. Bavaev <jesoba7@gmail.com>
2016-02-12 11:29:05 +1000

    fixed view performance

--- spec/features/dashboard/booking_flow_for_performer_spec.rb
+++ spec/features/dashboard/booking_flow_for_performer_spec.rb
@@ -4,18 +4,26 @@ require 'rails_helper'
 # booking requests, so that user who have booked with me know if I’m available or not
 
 feature 'Booking flow for performer' do
+  let(:show) { create :show, user: user }
+  let(:user) { FactoryGirl.create(:user) }
+  let!(:booking) { create(:booking, show: show, user: user, status: 1, date: 1.day.from_now) }
+  let!(:pending_booking) { create(:booking, show: show, user: user, status: 2, date: 1.day.from_now) }
 
-  scenario 'link to My Performances page' do
-    user = FactoryGirl.create(:user)
-    show = create :show, user: user
-    bookings = create_list(:booking, 2, show: show, user: user, status: 1, date: 1.day.from_now)
-
-    # When logged in and on homepage, performer can click on his name on the upper-right corner and select in the pop-up “Mes performances”
+  before(:each) do
     signin(user.email, user.password)
     click_link "My Performances"
+  end
 
-    #
+  scenario 'link to My Performances page' do
     expect(page).to have_css('.performance', count: 2)
+  end
+
+  # As a performer, I should be able to accept booking requests,
+  # so I can let the booker now I’m available to perform at his place
+  scenario 'Performer accepts booking flow', js: true do
+    within "#performance_#{pending_booking.id}" do
+      click_button 'Accept the application'
+    end
   end
 
 end
