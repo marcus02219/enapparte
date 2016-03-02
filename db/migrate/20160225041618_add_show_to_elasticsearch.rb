87e4588f056ea3c4a97dd9f2f78d8b122fe3abfd
csolg <jesoba7@gmail.com>
2016-03-02 18:32:39 +1000

    fixed show import to elasticsearch

--- db/migrate/20160225041618_add_show_to_elasticsearch.rb
+++ db/migrate/20160225041618_add_show_to_elasticsearch.rb
@@ -1,6 +1,6 @@
 class AddShowToElasticsearch < ActiveRecord::Migration
   def change
-    Show.import
+    # Show.import
     Show.__elasticsearch__.create_index! force: true
     Show.__elasticsearch__.refresh_index!
   end
