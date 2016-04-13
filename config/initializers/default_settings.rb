28b5f76d1ad1aa5bf925dbfee6e877a889097e54
csolg <jesoba7@gmail.com>
2016-04-13 10:50:51 +1000

    fixed default_from_email

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -1,3 +1,3 @@
 if Setting.table_exists?
-  Setting.save_default :default_from_email, '86d18b807e91e6d4a8bc3cf6bab924d2@inbound.postmarkapp.com'
+  Setting.save_default :default_from_email, 'no-reply@enapparteparis.com'
 end
