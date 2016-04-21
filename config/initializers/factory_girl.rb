af000fed09e146cccec10f22407faf41c7260ba2
csolg <jesoba7@gmail.com>
2016-04-22 01:12:09 +1000

    fixed for production run

--- config/initializers/factory_girl.rb
+++ config/initializers/factory_girl.rb
@@ -1,9 +1,8 @@
-ActionDispatch::Callbacks.after do
-  # Reload the factories
-  return unless (Rails.env.development? || Rails.env.test?)
-
-  unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
-    FactoryGirl.factories.clear
-    FactoryGirl.find_definitions
+if (Rails.env.development? || Rails.env.test?)
+  ActionDispatch::Callbacks.after do
+    unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
+      FactoryGirl.factories.clear
+      FactoryGirl.find_definitions
+    end
   end
 end
