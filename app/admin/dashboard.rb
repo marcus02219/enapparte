9910203b79d6a5139a8ca50e3f847079a4430630
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-11 21:40:16 +0700

    Fix links on Admin Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,18 +1,17 @@
-ActiveAdmin.register_page "Dashboard" do
+ActiveAdmin.register_page 'Dashboard' do
+  menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
-
-  content title: proc{ I18n.t("active_admin.dashboard") } do
+  content title: proc { I18n.t('active_admin.dashboard') } do
     columns do
       column do
-        panel "Recent Bookings" do
+        panel 'Recent Bookings' do
           ul do
             Booking.last(20).map do |comment|
-              li link_to(admin_booking_path(comment))
+              li link_to(admin_booking_path(comment), admin_booking_path(comment))
             end
           end
         end
       end
     end
-  end # content
+  end
 end
