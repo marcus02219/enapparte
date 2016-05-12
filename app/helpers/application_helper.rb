6cfb166441e8caaedc6684ff853f4e36181c1592
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-12 10:47:15 +0700

    Refactor ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-
-  def angular_templates templates_path
+  def angular_templates(templates_path)
     templates = Dir[templates_path + '**/*'].select { |f| File.file? f }
-    templates.inject("") do |js, template|
-      rendered = render file: template
-      template_id = Pathname.new(template.gsub(/\.(\w+)$/, '')).relative_path_from(Pathname.new(templates_path))
-      js += "<script type='text/ng-template' id='#{template_id.to_s}'>#{rendered}</script>"
+    templates.inject('') do |js, template|
+      rendered = render(file: template)
+      template_id = Pathname.new(template.gsub(/\.(\w+)$/, ''))
+                            .relative_path_from(Pathname.new(templates_path))
+      js + content_tag(:script, rendered, id: template_id,
+                                          type: 'text/ng-template')
     end
   end
-
 end
