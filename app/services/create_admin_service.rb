8405ae2b20653414a3efca4cd69e04a86761a078
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-12 13:48:54 +0700

    Refactor CreateAdminService

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,10 +1,11 @@
 class CreateAdminService
-  def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email, firstname: 'Admin', surname: 'Admin', gender: 1, phone_number: "123-456-7890") do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
-        user.role = 0
-        # user.confirm!
-      end
+  def perform
+    User.find_or_create_by!(email: Rails.application.secrets.admin_email,
+                            firstname: 'Admin', surname: 'Admin', gender: 1,
+                            phone_number: '123-456-7890') do |user|
+      user.password = Rails.application.secrets.admin_password
+      user.password_confirmation = Rails.application.secrets.admin_password
+      user.role = 0
+    end
   end
 end
