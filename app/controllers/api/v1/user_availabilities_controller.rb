72f09275ca4f0510b99da66e0bbc871883a081b6
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-13 16:24:12 +0700

    Add UserAvailabilitiesControler

---
+++ app/controllers/api/v1/user_availabilities_controller.rb
@@ -0,0 +1,35 @@
+module Api
+  module V1
+    class UserAvailabilitiesController < Api::BaseController
+      before_action :authenticate_user!
+
+      def index
+        @availabilities = current_user.availabilities
+        respond_with :api, :v1, @availabilities
+      end
+
+      def show
+        @availability = current_user.availabilities.find_by(id: params[:id])
+        respond_with :api, :v1, @availability
+      end
+
+      def create
+        @availability = current_user.availabilities.where(availability_params)
+                                    .first_or_create
+        respond_with :api, :v1, @availability
+      end
+
+      def destroy
+        @availability = current_user.availabilities.find_by(id: params[:id])
+        @availability.destroy
+        respond_with :api, :v1, @availability
+      end
+
+      private
+
+      def user_availability_params
+        params.require(:availability).permit(:available_at)
+      end
+    end
+  end
+end
