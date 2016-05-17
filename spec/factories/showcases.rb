3b26848042e74151bc7350e516c39d8a907828b5
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-17 19:11:35 +0700

    Add Showcase factory

---
+++ spec/factories/showcases.rb
@@ -0,0 +1,17 @@
+FactoryGirl.define do
+  factory :showcase do
+    url 'http://yandex.ru'
+  end
+end
+
+# == Schema Information
+#
+# Table name: showcases
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  type       :string
+#  url        :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
