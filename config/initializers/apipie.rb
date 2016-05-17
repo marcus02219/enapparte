162780a8c16ac45f5168d92be272d0cc3c163dce
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-17 22:49:55 +0700

    Configure api-pie

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,14 @@
 Apipie.configure do |config|
-  config.app_name                = "Enapparte"
-  config.api_base_url            = "/api"
-  config.doc_base_url            = "/apipie"
-  # where is your API defined?
-  config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.app_name = 'Enapparte'
+  config.api_base_url = '/api'
+  config.doc_base_url = '/apidoc'
+  config.reload_controllers = Rails.env.development?
+  config.api_controllers_matcher = File.join(Rails.root, 'app', 'controllers',
+                                             'api', '**', '**')
+  config.api_routes = Rails.application.routes
+  config.default_version = 'V1'
+  config.validate = true
+  config.default_locale = 'en'
+  config.markup = Apipie::Markup::Markdown.new
+  config.validate = false
 end
