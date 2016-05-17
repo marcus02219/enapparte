63ffcb7ea882bdc43abae3fceb2e0b3b89f4fff2
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-17 20:01:27 +0700

    Add Showcases API

---
+++ app/controllers/api/v1/showcases_controller.rb
@@ -0,0 +1,40 @@
+module Api
+  module V1
+    class ShowCasesController < Api::BaseController
+      before_action :authenticate_user!
+
+      def index
+        @showcases = current_user.showcases
+        respond_with :api, :v1, @showcases
+      end
+
+      def create
+        @showcase = current_user.showcases.create(showcase_params)
+        respond_with :api, :v1, @showcase
+      end
+
+      def show
+        @showcase = current_user.showcases.find(params[:id])
+        respond_with :api, :v1, @showcase
+      end
+
+      def update
+        @showcase = current_user.showcases.find(params[:id])
+        @showcase.update_attributes showcase_params
+        respond_with :api, :v1, @showcase
+      end
+
+      def destroy
+        @showcase = current_user.showcases.find(params[:id])
+        @showcase.destroy
+        respond_with :api, :v1, @showcase
+      end
+
+      private
+
+      def showcase_params
+        params.require(:show).permit(:kind, :url)
+      end
+    end
+  end
+end
