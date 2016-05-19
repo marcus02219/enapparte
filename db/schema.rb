ac908c208e49cd6d78a665dc034b22d1c1d9edd8
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-19 17:35:10 +0700

    Remvoe art and date_at from shows

--- db/schema.rb
+++ db/schema.rb
@@ -156,15 +156,12 @@ ActiveRecord::Schema.define(version: 20160519064950) do
     t.datetime "published_at"
     t.integer  "cover_picture_id"
     t.integer  "user_id"
-    t.integer  "art_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.float    "rating"
     t.boolean  "price_person"
-    t.datetime "date_at"
   end
 
-  add_index "shows", ["art_id"], name: "index_shows_on_art_id", using: :btree
   add_index "shows", ["cover_picture_id"], name: "index_shows_on_cover_picture_id", using: :btree
   add_index "shows", ["user_id"], name: "index_shows_on_user_id", using: :btree
 
