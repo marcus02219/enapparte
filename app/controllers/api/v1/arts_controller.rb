739d03eff9e9836be7131ef1e8d1dd8c411700b4
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-19 11:37:35 +0700

    Refactor ArtsController

--- app/controllers/api/v1/arts_controller.rb
+++ app/controllers/api/v1/arts_controller.rb
@@ -1,8 +1,12 @@
-class Api::V1::ArtsController < Api::BaseController
-  skip_before_filter :authenticate_user!, only: :index
-  def index
-    @arts = Art.order('title').all
-    respond_with :api, :v1, @arts
-  end
+module Api
+  module V1
+    class ArtsController < Api::BaseController
+      skip_before_action :authenticate_user!, only: :index
 
+      def index
+        @arts = Art.order('title').all
+        respond_with :api, :v1, @arts
+      end
+    end
+  end
 end
