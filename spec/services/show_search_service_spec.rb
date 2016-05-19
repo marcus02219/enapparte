3442f7a850a0b41203efd02b892f8e3abaf55422
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-19 08:35:56 +0700

    Remove art from search tests

--- spec/services/show_search_service_spec.rb
+++ spec/services/show_search_service_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 describe ShowSearchService, type: :service do
-  let(:art_video) { create(:art, title: 'Video') }
-  let(:art_music) { create(:art, title: 'Music') }
   let!(:show_song) do
-    create(:show, title: 'First Song', price: 60, art: art_music, active: true)
+    create(:show, title: 'First Song', price: 60, active: true)
   end
   let!(:show_bit) do
-    create(:show, title: 'Bit Song', price: 80, art: art_music, active: true)
+    create(:show, title: 'Bit Song', price: 80, active: true)
   end
   let!(:show_clip) do
-    create(:show, title: 'Clip', price: 100, art: art_video, active: true)
+    create(:show, title: 'Clip', price: 100, active: true)
   end
 
   describe '#results' do
@@ -37,18 +35,8 @@ describe ShowSearchService, type: :service do
       end
     end
 
-    describe 'filter by arts' do
-      context 'arts exist' do
-        let(:params) { { arts: "[#{art_video.id}]" } }
-
-        it 'returns filtered shows' do
-          expect(subject).to contain_exactly(show_clip)
-        end
-      end
-    end
-
     context 'all params empty' do
-      let(:params) { { query: '', price_min: '', price_max: '', arts: '' } }
+      let(:params) { { query: '', price_min: '', price_max: '' } }
 
       it 'returns all shows' do
         expect(subject).to contain_exactly(show_song, show_bit, show_clip)
