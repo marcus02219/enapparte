2aebdbec64ed16a16cf3c9c0e2a5b8fe28a0035c
“Marcus;export GIT_AUTHOR_EMAIL=marcus02219@gmail.com;export GIT_COMMITTER_NAME=“marcus <authorEmail@example.com>
2016-05-20 13:40:29 +0700

    Add API docs for Arts API

--- app/controllers/api/v1/arts_controller.rb
+++ app/controllers/api/v1/arts_controller.rb
@@ -3,6 +3,34 @@ module Api
     class ArtsController < Api::BaseController
       skip_before_action :authenticate_user!, only: :index
 
+      resource_description do
+        short 'Arts'
+      end
+
+      api! 'List of Arts'
+      description <<-EOS
+        ## Description
+        Returns all arts.
+      EOS
+      example <<-EOS
+        [
+          {
+            "created_at": "2016-04-29T05:07:20.897Z",
+            "description": "",
+            "id": 12,
+            "title": "Gastronomie",
+            "updated_at": "2016-05-10T14:45:23.815Z"
+          },
+          {
+            "created_at": "2016-03-09T07:21:58.115Z",
+            "description": "Laborum minima laboriosam. Non placeat quis. Illum omnis et.",
+            "id": 8,
+            "title": "Insolite",
+            "updated_at": "2016-04-29T05:05:54.074Z"
+          }
+        ]
+      EOS
+
       def index
         @arts = Art.order('title').all
         respond_with :api, :v1, @arts
